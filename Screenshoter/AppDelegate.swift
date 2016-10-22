97870e71037d49eb7cf1afb5ef197666312ba423
Svyatoshenko Misha <megal06@nightmail.ru>
2016-10-22 13:55:02 +0300

    save a screenshot to ~/Desktop/screenshot.png

--- Screenshoter/AppDelegate.swift
+++ Screenshoter/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import Cocoa
+import Quartz
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -14,7 +15,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
+
+		let displayID = CGMainDisplayID()
+		let img = CGDisplayCreateImage(displayID)!
+
+		let writePng = URL(fileURLWithPath: NSHomeDirectory() + "/Desktop/screenshot.png")
+		let dest = CGImageDestinationCreateWithURL(writePng as CFURL, kUTTypePNG, 1, nil)!
+
+		CGImageDestinationAddImage(dest, img, nil)
+		guard CGImageDestinationFinalize(dest) else {
+			fatalError("cannot save screenshot");
+		}
 		// Insert code here to initialize your application
+
+		NSApp.terminate(self)
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
