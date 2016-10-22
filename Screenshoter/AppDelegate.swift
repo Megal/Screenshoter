53792e843ebc4b12bf3e408bf45c181ffed9ca07
Svyatoshenko Misha <megal06@nightmail.ru>
2016-10-22 12:11:41 +0300

    Initial Commit

---
+++ Screenshoter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Screenshoter
+//
+//  Created by Svyatoshenko "Megal" Misha on 2016-10-22.
+//  Copyright © 2016年 Svyatoshenko "Megal" Misha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Screenshoter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Screenshoter
+//
+//  Created by Svyatoshenko "Megal" Misha on 2016-10-22.
+//  Copyright © 2016年 Svyatoshenko "Megal" Misha. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
